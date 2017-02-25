f6652b05e2999a52699c79da0a14e5f153eab2cf
Luis <theluisrivera@gmail.com>
2017-02-24 22:25:41 -0800

    index user email

--- db/migrate/20170225010809_create_users.rb
+++ db/migrate/20170225010809_create_users.rb
@@ -3,6 +3,7 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :email
+      t.index :email
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -19,4 +19,6 @@ ActiveRecord::Schema.define(version: 20170225010809) do
     t.datetime "updated_at", null: false
   end
 
+  add_index "users", ["email"], name: "index_users_on_email"
+
 end
